7e7e6c2fb4f36a63c79b94358b73d4b9c263e3c1
bigtimer-dev <aljnoma@gmail.com>
2025-12-15 21:56:40 -0400

    more extensions

--- extensions.py
+++ extensions.py
@@ -1,4 +1,11 @@
-exe_file = ".exe"
-zip_file = ".zip"
-txt_file = ".txt"
-python_file = ".py"
+extensions_dict = {
+    "exe_file": ".exe",
+    "zip_file": ".zip",
+    "txt_file": ".txt",
+    "python_file": ".py",
+    "c_files": ".c",
+    "c_header_files": ".h",
+    "png_pictures": ".png",
+    "jpg_pictures": ".jpg",
+    "gif_pictures": ".gif_pictures",
+}
