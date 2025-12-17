422ebcb20abef0745953376e1924867f59a83df3
bigtimer-dev <aljnoma@gmail.com>
2025-12-16 21:40:50 -0400

    add function to return list of items in directory

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import os
+from extensions import extensions_dict
+
+
+def list_dir(dir):
+    if os.path.isdir(dir):
+        return os.listdir(dir)
+    else:
+        raise Exception(f"Error: {dir} is not a directory")
+
+
 def main():
     print("Hello from files-organizer!")
 
