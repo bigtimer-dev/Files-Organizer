455a32c3209969ffca5f46eddb849d4096b6371c
bigtimer-dev <aljnoma@gmail.com>
2025-12-17 11:19:37 -0400

    checking

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from extensions import extensions_dict
 
 
@@ -7,6 +8,12 @@ def list_dir(dir):
         return os.listdir(dir)
     else:
         raise Exception(f"Error: {dir} is not a directory")
+
+
+def move_contents(src, dest):
+    contents = list_dir(src)
+    for content in contents:
+        full_path_to_content = os.path.join(src, content)
 
 
 def main():
