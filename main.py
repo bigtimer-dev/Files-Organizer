fe27898ca5f9f3871de19291731dd94e34bb926c
bigtimer-dev <aljnoma@gmail.com>
2025-12-18 20:30:27 -0400

    add some

--- main.py
+++ main.py
@@ -11,9 +11,14 @@ def list_dir(dir):
 
 
 def move_contents(src, dest):
-    contents = list_dir(src)
-    for content in contents:
-        full_path_to_content = os.path.join(src, content)
+    if os.path.exists(dest):
+        contents = list_dir(src)
+        for content in contents:
+            full_path_to_content = os.path.join(src, content)
+            for extension in extensions_dict.values():
+                if full_path_to_content.endswith(extension):
+
+
 
 
 def main():
